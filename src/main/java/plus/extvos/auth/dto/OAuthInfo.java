44a2fad5c80ceb8f329ccce6eb12d88a8bdca215
Mingcai SHEN <archsh@gmail.com>
2022-09-08 17:04:49 +0800

    Updated for Serializable.

--- src/main/java/plus/extvos/auth/dto/OAuthInfo.java
+++ src/main/java/plus/extvos/auth/dto/OAuthInfo.java
@@ -8,7 +8,7 @@ import java.util.Map;
  *
  * @author shenmc
  */
-public class OAuthInfo {
+public class OAuthInfo implements Serializable {
     private Serializable id;
     private Serializable userId;
     private String provider;
--- src/main/java/plus/extvos/auth/dto/OAuthResult.java
+++ src/main/java/plus/extvos/auth/dto/OAuthResult.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * @author Mingcai SHEN
  */
-public class OAuthResult {
+public class OAuthResult  implements Serializable {
     private String session;
     private Serializable userId;
     private String username;
